13ae9c0664d1d4cc6ea767e06492dbd83a425d59
Diego Vargas <dialvaro30@gmail.com>
2019-06-29 16:02:43 -0500

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,12 +5,12 @@
     <div class="row justify-content-center py-5">
         <div class="col-md-5 mx-auto">
             <div class="card">
-                <div class="card-header">
-                    <div class="col-md-12 text-center">
-                        <h4><i class="fas fa-meh-rolling-eyes"></i> {{ __('Reset Password') }}</h4>
+                <div class="col-md-12 text-center py-3">
+                    <div class="text-center">
+                        <i class="fas fa-lock fa-3x"></i> 
                     </div>
+                    <span>¿Tienes problemas para iniciar sesión?</span>
                 </div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -20,12 +20,11 @@
 
                     <form method="POST" action="{{ route('password.email') }}" novalidate>
                         @csrf
-
+                        <div class="text-center py-2">
+                            <span class="text-muted">Ingresa tu correo electrónico y te enviaremos un enlace para recuperar el acceso a tu cuenta.</span>
+                        </div>
                         <div class="form-group row">
                             <div class="input-group col-md-12">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" id="basic-addon1"><i class="fas fa-at"></i></span>
-                                </div>
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus placeholder="{{ __('E-Mail Address') }}">
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
@@ -37,20 +36,25 @@
 
                         <div class="form-group mb-0">
                             <div class="col-12 ">
-                                <button type="submit" class="btn btn-success btn-block">
-                                    <i class="fas fa-check"></i> {{ __('Send Password Reset Link') }}
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    <i class="fas fa-check"></i> Enviar enlace
                                 </button>
                             </div>
                         </div>
                         <div class="dropdown-divider"></div>
                         <div class="form-group mb-0">
                             <div class="col-md-12 text-center">
-                                <a class="btn btn-block btn-primary tx-tfm" href="{{ route('login') }}">
-                                    <i class="fas fa-sign-in-alt"></i> Ingresar
-                                </a>
+                                <span>
+                                    <a class="mb-2 text-dark" href="{{ route('register') }}"><strong>Crea cuenta nueva</strong> </a>
+                                </span>
                             </div>
                         </div>
                     </form>
+                </div>
+                <div class="card-footer text-center">
+                    <a class=" btn btn-outline-secondary tx-tfm text-dark" href="{{ route('login') }}">
+                        <i class="fas fa-arrow-left"></i> Volver a inicio de sesión
+                    </a>
                 </div>
             </div>
         </div>
